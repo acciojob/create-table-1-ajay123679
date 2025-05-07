247b3d5eff1e8a2b66327f8a163a5c33ddf413c1
ajay123679 <65329197+ajay123679@users.noreply.github.com>
2025-05-07 10:15:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
+  let tbl=document.getElementById("sampleTable");
+  let nrow=tbl.insertRow(0);
+  let cell1=nrow.insertCell(0);
+  let cell2=nrow.insertCell(1);
+  cell1.textContent="New Cell1";
+  cell2.textContent="New Cell2";
   
 }
